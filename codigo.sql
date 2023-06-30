517bd38043bc3fc18cd5a32b268abfb9e07e56c1
w4lt3rdm <138255443+w4lt3rdm@users.noreply.github.com>
2023-06-30 17:48:19 -0300

    subo sentencias select
    clase 30/06/2023

---
+++ codigo.sql
@@ -0,0 +1,31 @@
+/* 
+SELECT
+Mostrar todos los registros de la tabla de movies.
+Mostrar el nombre, apellido y rating de todos los actores.
+Mostrar el título de todas las series.
+
+WHERE Y ORDER BY
+Mostrar el nombre y apellido de los actores cuyo rating sea mayor a 7,5.
+Mostrar el título de las películas, el rating y los premios de las películas con un rating mayor a 7,5 y con más de dos premios.
+Mostrar el título de las películas y el rating ordenadas por rating en forma ascendente.
+
+BETWEEN y LIKE
+Mostrar el título y rating de todas las películas cuyo título incluya Toy Story.
+Mostrar a todos los actores cuyos nombres empiecen con Sam.
+Mostrar el título de las películas que salieron entre el ‘2004-01-01’ y ‘2008-12-31’.
+
+
+SELECT * FROM movies;
+SELECT first_name, last_name, rating FROM actors;
+SELECT title from series;
+SELECT first_name, last_name FROM actors WHERE rating > 7.5;
+SELECT title, rating FROM movies WHERE rating >=7.5 AND awards >2;
+SELECT title, rating FROM movies WHERE title LIKE '%Toy Story%';
+SELECT * FROM actors WHERE first_name LIKE 'Sam%';
+SELECT * FROM movies WHERE release_date BETWEEN '2004-01-01' AND '2008-12-31' ;
+*/
+
+
+
+
+
